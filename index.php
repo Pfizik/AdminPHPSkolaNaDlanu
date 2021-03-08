c56f6e8492dd1fda36dd1121d75e3bba9b9a933b
Pfizik <viksi2806@gmail.com>
2021-03-08 06:00:53 +0100

    Input and upload

--- index.php
+++ index.php
@@ -1,3 +1,74 @@
-<?php 
-    echo "hello";
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>  
+
+<?php
+// define variables and set to empty values
+$passwordErr = $emailErr = $inputErr = "";
+$password = $email = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+
+  if (empty($_POST["password"])) {
+    $passwordErr = "Password is required";
+  } else {
+    $password = test_input($_POST["password"]);
+    // check if password only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$password)) {
+      $passwordErr = "Only letters and white space allowed";
+    }
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+if($email == "viksi2806@gmail.com" && $password == "viksinesto"){
+    header("Location: /select.php"); 
+    exit();
+}else{
+    $inputErr = "Invalid creditentials!";
+    echo $inputErr;
+}
 ?>
+
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Password: <input type="text" name="password" value="<?php echo $password;?>">
+  <span class="error">* <?php echo $passwordErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">
+  <span class="error"><?php echo $passwordErr;?></span>
+</form>
+
+<?php
+echo $inputErr;
+?>
+
+<br>
+© 2020-<?php echo date("Y");?>
+
+</body>
+</html>
